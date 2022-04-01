88fde22d4d25d720696b1c5bd7ec257c15577ce1
Umar Farooq <37244088+umarf786@users.noreply.github.com>
2022-04-01 15:38:05 +0100

    test
    test

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('You are on the homepage');
+  res.send('test');
 });
 
 module.exports = router;
